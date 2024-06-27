81ebee948093d51199e1290203b98ed9599ef0da
Charles Moore <moorec@amazon.com>
2024-06-27 23:09:23 +0000

    fix!: delete workers from non-autoscaling fleets
    Signed-off-by: Charles Moore <moorec@amazon.com>

--- test/unit/deadline/test_worker.py
+++ test/unit/deadline/test_worker.py
@@ -14,14 +14,16 @@ from moto import mock_ec2, mock_iam, mock_s3, mock_ssm
 
 from deadline_test_fixtures.deadline import worker as mod
 from deadline_test_fixtures import (
-    CodeArtifactRepositoryInfo,
     CommandResult,
     DeadlineWorkerConfiguration,
     DockerContainerWorker,
     EC2InstanceWorker,
     PipInstall,
+    CodeArtifactRepositoryInfo,
     OperatingSystem,
     S3Object,
+    Fleet,
+    Farm,
 )
 
 
@@ -62,7 +64,7 @@ def region(boto_config: dict[str, str]) -> str:
 def worker_config(region: str) -> DeadlineWorkerConfiguration:
     return DeadlineWorkerConfiguration(
         farm_id="farm-123",
-        fleet_id="fleet-123",
+        fleet=Fleet(id="fleet_123", farm=Farm(id="farm-123")),
         region=region,
         user="test-user",
         group="test-group",
@@ -157,6 +159,7 @@ class TestEC2InstanceWorker:
             s3_client=boto3.client("s3"),
             ec2_client=boto3.client("ec2"),
             ssm_client=boto3.client("ssm"),
+            deadline_client=boto3.client("deadline"),
             configuration=worker_config,
         )
 
